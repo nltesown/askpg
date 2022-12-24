10667d9828d8e3c14d94570a6cc4d31f9b265259
NLTESOWN <nlte@fastmail.com>
2022-12-24 10:43:59 +0100

    Cleanup

--- src/routes/deepzoom/+page.svelte
+++ src/routes/deepzoom/+page.svelte
@@ -1,42 +1,42 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	let OpenSeadragon: Object;
-	onMount(async () => {
-		OpenSeadragon = await import('openseadragon');
+  import { onMount } from "svelte";
+  let OpenSeadragon: Object;
+  onMount(async () => {
+    OpenSeadragon = await import("openseadragon");
 
-		// http://openseadragon.github.io/docs/OpenSeadragon.html#.Options
-		new OpenSeadragon.Viewer({
-			id: 'viewer',
-			tileSources: 'img/deepzoom/peardice/output.dzi',
-			alwaysBlend: false,
-			autoHideControls: true,
-			// wrapHorizontal: true,
-			// wrapVertical: true,
-			minZoomImageRatio: 0.5,
-			maxZoomPixelRatio: 3,
-			showNavigationControl: false,
-			showNavigator: false,
-			mouseNavEnabled: true,
-			visibilityRatio: 0.5,
-			zoomPerClick: 1.25,
-			zoomPerScroll: 1.25,
-			animationTime: 1,
-			useCanvas: true,
-			autoResize: true
-		});
-	});
+    // http://openseadragon.github.io/docs/OpenSeadragon.html#.Options
+    new OpenSeadragon.Viewer({
+      id: "viewer",
+      tileSources: "img/deepzoom/peardice/output.dzi",
+      alwaysBlend: false,
+      autoHideControls: true,
+      wrapHorizontal: true,
+      wrapVertical: true,
+      minZoomImageRatio: 0.5,
+      maxZoomPixelRatio: 3,
+      showNavigationControl: false,
+      showNavigator: false,
+      mouseNavEnabled: true,
+      visibilityRatio: 0.5,
+      zoomPerClick: 1.25,
+      zoomPerScroll: 1.25,
+      animationTime: 2,
+      useCanvas: true,
+      autoResize: true
+    });
+  });
 </script>
 
 <div id="viewer" />
 
 <style>
-	#viewer {
-		position: absolute;
-		top: 0;
-		left: 0;
-		width: 100vw;
-		height: 100vh;
-		cursor: move;
-		background-color: rgb(191, 255, 126);
-	}
+  #viewer {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    cursor: move;
+    background-color: rgb(191, 255, 126);
+  }
 </style>
